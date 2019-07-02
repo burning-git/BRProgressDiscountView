d37f7f001f801a9ab9872bc7b700691b23db3ce0
burning_git <burning_git@163.com>
2019-07-02 18:27:24 +0800

    Initial Commit

---
+++ BRProgressDiscountViewProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BRProgressDiscountViewProject
+//
+//  Created by git burning on 2019/7/2.
+//  Copyright © 2019 git burning. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
